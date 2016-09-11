63bfcbc89b6f07713583eda356cb440c29c3851f
hdillon <dillonhoraciodavid@gmail.com>
2016-09-11 20:27:06 -0300

    Sonidos + icono de la app

--- PianoTematico/www/js/controllers.js
+++ PianoTematico/www/js/controllers.js
@@ -25,4 +25,95 @@ angular.module('starter.controllers', [])
 
 .controller('JugarCtrl', function($scope, $ionicPopup, $state, $stateParams, $cordovaVibration,  $cordovaNativeAudio, $timeout) {
   $scope.usuario = angular.fromJson($stateParams);
+
+$scope.reproducirSonido = function(idSonido) {
+    switch(idSonido){
+        case 'caballo':
+          $scope.play('caballo');
+        break;
+        case 'leon':
+          $scope.play('leon');
+        break;
+        case 'becerro':
+          $scope.play('becerro');
+        break;
+        case 'pajarito':
+          $scope.play('pajarito');
+        break;
+        case 'oveja':
+          $scope.play('oveja');
+        break;
+        case 'gallo':
+          $scope.play('gallo');
+        break;
+        default:
+        break;
+      }
+  };
+
+/****FUNCIONES NATIVE AUDIO****/
+try{
+  if(!$scope.seCargaronLosSonidos){
+    $scope.seCargaronLosSonidos = true;
+  $cordovaNativeAudio
+      .preloadSimple('caballo', 'audio/caballo.mp3')
+      .then(function (msg) {
+        console.log(msg);
+      }, function (error) {
+        console.log(error);
+      });
+
+  $cordovaNativeAudio
+      .preloadSimple('leon', 'audio/leon.mp3')
+      .then(function (msg) {
+        console.log(msg);
+      }, function (error) {
+        console.log(error);
+      });
+
+  $cordovaNativeAudio
+      .preloadSimple('becerro', 'audio/becerro.mp3')
+      .then(function (msg) {
+        console.log(msg);
+      }, function (error) {
+        console.log(error);
+      });
+
+  $cordovaNativeAudio
+      .preloadSimple('pajarito', 'audio/pajarito.mp3')
+      .then(function (msg) {
+        console.log(msg);
+      }, function (error) {
+        console.log(error);
+      });
+
+  $cordovaNativeAudio
+      .preloadSimple('oveja', 'audio/oveja.mp3')
+      .then(function (msg) {
+        console.log(msg);
+      }, function (error) {
+        console.log(error);
+      });
+
+  $cordovaNativeAudio
+      .preloadSimple('gallo', 'audio/gallo.mp3')
+      .then(function (msg) {
+        console.log(msg);
+      }, function (error) {
+        console.log(error);
+      });
+  }
+}catch(err){
+  console.log("No es un dispositivo mobile");
+}
+
+$scope.play = function (sound) {
+  try{
+  $cordovaNativeAudio.play(sound);
+  }catch(err){
+    console.log("No es un dispositivo mobile");
+  }
+};
+/****FIN FUNCIONES NATIVE AUDIO****/
+
 });
